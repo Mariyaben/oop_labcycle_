e7b89fd802bab385b48159c8b688735fdf6db39f
Mariyaben <98140958+Mariyaben@users.noreply.github.com>
2022-04-18 09:11:29 +0530

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,124 +1,136 @@
-#include <iostream>
-#include <cstring>
-
+#include<iostream>
+#include<cstring>
 using namespace std;
-
-class strng
+class String
 {
-	char *str;
-	int length;
+    char* str;
+    int length;
 public:
-	strng(){
-		length=0;
-		str=new char[length+1];
-	}
-	strng(const char *s)
-	{
-		length=strlen(s);
-		str = new char[length+1];
-		strcpy(str,s);
-	}
-        void CNT(void){count = 0;}
-	void get(){cin>>str;}
-	void display(){cout<<str<<endl;}
-	void concatenate(strng a,strng b);
-	void stlength();
-	void compare(strng s1,strng S2);
+    String()
+    {
+        length = 0;
+        str = new char[length + 1];
+    }
+    String(const char* s)
+    {
+        length = strlen(s);
+        str = new char[length + 1];
+        strcpy(str, s);
+    }
+    void get_string()
+    {
+        cin >> str;
+    }
+    void display()
+    {
+        cout << str << endl;
+    }
+    void concat(String a, String b);
+    void stlength();
+    void compare(String s1, String s2);
 };
-
-void strng::concatenate(strng a,strng b)
+void String::concat(String a, String b)
 {
-	length=a.length+b.length;
-	delete str;
-	str=new char[length+1];
-	strcpy(str,a.str);
-	strcat(str,b.str);
+    length = a.length + b.length;
+    delete str;
+    str = new char[length + 1];
+    strcpy(str, a.str);
+    strcat(str, b.str);
+    cout << endl;
 }
-
-void strng::stlength()
+void String::stlength()
 {
-	cout<<strlen(str);
+    cout << strlen(str);
+    cout << endl;
 }
-
-void strng::compare(strng s1, strng s2)
+void String::compare(String s1, String s2)
 {
-	int m,n;
-	m=strlen(s1.str);
-	n=strlen(s2.str);
-        if(m==n)
-                cout<<s1.str<<" is the same length as "<<s2.str<<"\n";
-	else if(m<=n)
-		cout<<s1.str<<" is smaller than "<<s2.str<<"\n";
-	else
-		cout<<s2.str<<" is smaller than "<<s1.str<<"\n";
+    int m, n;
+    m = strlen(s1.str);
+    n = strlen(s2.str);
+    if (m < n)
+        cout << s1.str << " is smaller than " << s2.str << "\n";
+    if (m = n)
+        cout << s1.str << " is equal to " << s2.str << "\n";
+    else
+        cout << s2.str << " is smaller than " << s2.str << "\n";
 }
-
 int main()
 {
-	strng Str1, Str2, Str3;
-cout<<"\n\n___________________________________________________________________________";
-	cout<<"\nWelcome!!";
+    int ch;
+    string rp;
+    String Str1, Str2, Str3;
+    {
+        while (true)
+        {   cout<<"******************************\n";
+	    cout << "You can do the following:\n";
+            cout << "Operations on Strings:\n";
+            cout << "1.String concatenation\n";
+            cout << "2.Lengths of String \n";
+            cout << "3.Comparison of strings\n";
+            cout << "Enter choice:\n";
+            cin >> ch;
 
-strng order;
-order.CNT();
-int x;
-do
-{
-cout<<"-----------------------------------------------------------------------------------";
-cout<<"\n You can do the following : ";
-cout<<"\n 1. Add an item ";
-cout<<"\n 2. Display total value ";
-cout<<"\n 3. Delete an item ";
-cout<<"\n 4. Display all item ";
-cout<<"\n 5. Quit ";
-cout<<"\n What is your Option: ";
-cout<<"\n--------------------------------------------------------------------------------------";
-cin>>x;switch(x)
-{
-case 1:
-order.concatenate();
-break;
-case 2:
-order.stlength();
-break;
-case 3:
-order.compare();
-break;
-case 4:
-order.display();
-break;
-case 5:
-break;
-default :
-cout<<"\nError in input; try again\n";
+            if (ch == 1)
+            {
+                cout << "1.String concatenation\n";
+               
+                cout << "Enter first string:\n";
+                Str1.get_string();
+                cout << "Enter second string:\n";
+                Str2.get_string();
+                cout << "First string is:";
+                Str1.display();
+                cout << "Second string is:";
+                Str2.display();
+                Str3.concat(Str1, Str2);
+                cout << "After concatenation:\n";
+                Str3.display();
+                cout << endl;
+            }
+            else if (ch == 2)
+            {
+                cout << "2.String comparison\n";
+                cout << "Enter first string:\n";
+                Str1.get_string();
+                cout << "Enter second string:\n";
+                Str2.get_string();
+                cout << "First string is:";
+                Str1.display();
+                cout << "Second string is:";
+                Str2.display();
+                cout << "\nLength of first string:";
+                Str1.stlength();
+                cout << "\nLength of second string:";
+                Str2.stlength();
+                cout << "\nLength of concatenated string:";
+                Str3.stlength();
+                cout << endl;
+            }
+            else if (ch == 3)
+
+            {
+                cout << "3.Comparison of strings\n";
+                cout << "Enter first string:\n";
+                Str1.get_string();
+                cout << "Enter second string:\n";
+                Str2.get_string();
+                cout << "First string is:";
+                Str1.display();
+                cout << "Second string is:";
+                Str2.display();
+                Str3.compare(Str1, Str2);
+            }
+
+            cout << "Do you want to continue(y/n): ";
+            cin >> rp;
+            if (rp == "y")
+                continue;
+            else
+                cout << "Exit";
+
+            return false;
+        }
+
+    }
 }
-}
-while(x != 5);
-
-
-	cout<<"\n\nEnter first string : ";
-	Str1.get();
-	cout<<"\nEnter second string : ";
-	Str2.get();
-cout<<"\n\n___________________________________________________________________________";
-	cout<<"\nFirst string : ";
-	Str1.display();
-	cout<<"\nSecond String is : ";
-	Str2.display();
-cout<<"\n\n___________________________________________________________________________";
-	Str3.concatenate(Str1,Str2);
-	cout<<"\n\nAfter concatenation : ";
-	Str3.display();
-	cout<<"\n\nLength of first string : ";
-	Str1.stlength();
-	cout<<"\n\nlength of second string : ";
-	Str2.stlength();
-	cout<<"\n\nLength of concatenated string : ";
-	Str3.stlength();
-	cout<<"\n\nComparison of string : ";
-	Str3.compare(Str1,Str2);
-cout<<"\n\n___________________________________________________________________________"<<endl;
-	return 0;
-
-}
-
